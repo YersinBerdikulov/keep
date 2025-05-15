7af203380f45fc4e19b5bce9fb267ecc556eb892
Yersin Berdikulov <221225@astanait.edu.kz>
2025-05-15 21:00:07 +0500

    configs not finished

--- lib/core/constants/appwrite_config.dart
+++ lib/core/constants/appwrite_config.dart
@@ -1,16 +1,16 @@
 class AppwriteConfig {
   static String endPoint = 'https://cloud.appwrite.io/v1';
-  static String projectId = '669e542d000c80288e0e';
-  static String databaseId = '6687c9910025465bb7d3';
-  static String boxCollection = '6697b86600200a44a26d';
-  static String boxUserCollection = '66991e8b0006f7a5e1a6';
-  static String groupCollection = '6687cc90003689fd4849';
-  static String groupUserCollection = '66991fab00086ada0493';
-  static String expenseCollection = '6697cc00000f154bb971';
-  static String expenseUserCollection = '66991f18003c857748fd';
-  static String usersCollection = '6687c9bc0026b33e80c1';
-  static String userFriendCollection = '66992010001fe10595f9';
-  static String imagesBucket = '6697ba71001fc29781b9';
+  static String projectId = '6825a463002d35b87b9c';
+  static String databaseId = '682606330003d48f1808';
+  static String boxCollection = '68260679002e501c45f6';
+  static String boxUserCollection = '682606af000d51ba45b3';
+  static String groupCollection = '6826071c00145957dc64';
+  static String groupUserCollection = '68260747001353af3782';
+  static String expenseCollection = '6826075600215b66ed3d';
+  static String expenseUserCollection = '682607700032d7528aa1';
+  static String usersCollection = '68260781000ac685fc36';
+  static String userFriendCollection = '6826078f001b57cc9d37';
+  static String imagesBucket = '682607b2003c4d883989';
   static String imageUrl(String imageId) =>
       'https://cloud.appwrite.io/v1/storage/buckets/$imagesBucket/files/$imageId/view?project=$projectId&mode=admin';
 }
